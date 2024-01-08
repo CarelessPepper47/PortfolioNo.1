6a19eb4d58fbc12c6ff24ae211277db3059f1ea5
MatiDev <mtsz.raszeja@gmail.com>
2024-01-08 16:05:58 +0100

    ostatni na dzis commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,26 @@ function App() {
 
   return (
     <>
-      <div className="flex justify-center transition-all hover:m-10">
-        <div className="m-4 h-10 w-10 bg-slate-400"></div>
-        <div className="h-10 w-10 bg-slate-400"></div>
-        <div className="h-10 w-10 bg-slate-400"></div>
-        <div className="h-10 w-10 bg-slate-400"></div>
-        <div className="h-10 w-10 bg-slate-400"></div>
-        <div className="h-10 w-10 bg-red-400"></div>
-        <div className="h-10 w-10 bg-red-400"></div>
-        <div className="h-10 w-10 bg-red-400"></div>
-        <div className="h-10 w-10 bg-red-400"></div>
-        <div className="m-4 h-10 w-10 bg-red-400"></div>
-        <div>
-          <div className="h-10 w-10 bg-slate-400"></div>
-          <div className="h-10 w-10 bg-slate-400"></div>
-          <div className="h-10 w-10 bg-slate-400"></div>
-          <div className="h-10 w-10 bg-slate-400"></div>
-          <div className="h-10 w-10 bg-red-400"></div>
-          <div className="h-10 w-10 bg-red-400"></div>
-          <div className="h-10 w-10 bg-red-400"></div>
-          <div className="h-10 w-10 bg-red-400"></div>
-        </div>
-      </div>
+      {/* hover:m-10 */}
+      <header className="m-4 flex bg-green-100 p-5 transition-all">
+        <nav className="flex w-full justify-around bg-blue-100 hover:justify-center">
+          <div>
+            <div>Logo</div>
+          </div>
+          <div className="flex">
+            <div className="">Home</div>
+            <div className="">About</div>
+            <div className="">Vehicle Models</div>
+            <div className="">Testimonials</div>
+            <div className="">Our Team</div>
+            <div className="">Contact</div>
+          </div>
+          <div>
+            <button className=" bg-white">Sign In</button>
+            <button className=" bg-red-700">Register</button>
+          </div>
+        </nav>
+      </header>
     </>
   );
 }
