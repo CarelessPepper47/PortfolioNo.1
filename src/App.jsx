35680b2488bcd03e47970e3b103dec85aae0fa5b
MatiDev <mtsz.raszeja@gmail.com>
2024-01-08 15:13:10 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  const [count, setCount] = useState(0);
+  let pawerl = "pawel";
+  let x = 2;
+
+  return (
+    <>
+      <div className="flex justify-center transition-all hover:m-10">
+        <div className="m-4 h-10 w-10 bg-slate-400"></div>
+        <div className="h-10 w-10 bg-slate-400"></div>
+        <div className="h-10 w-10 bg-slate-400"></div>
+        <div className="h-10 w-10 bg-slate-400"></div>
+        <div className="h-10 w-10 bg-slate-400"></div>
+        <div className="h-10 w-10 bg-slate-400"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
