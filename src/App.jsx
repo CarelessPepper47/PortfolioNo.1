41e05d8b84cab094e45ad4b06450d3b1d737e7d6
MatiDev <mtsz.raszeja@gmail.com>
2024-01-09 12:37:08 +0100

    zdjecie

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,10 @@ function App() {
       <header className="m-4 flex bg-green-100 p-5 transition-all">
         <nav className="flex w-full justify-around bg-blue-100 hover:justify-center">
           <div>
-            <div>Logo</div>
+            <div>
+              <img src="C:\Users\mtszr\Downloads\LogoAuto.jpg" alt="Logo" />
+              Logo
+            </div>
           </div>
           <div className="flex">
             <div className="">Home</div>
@@ -23,7 +26,9 @@ function App() {
           </div>
           <div>
             <button className=" bg-white">Sign In</button>
-            <button className=" bg-red-700">Register</button>
+            <button className=" bg-red-700 px-10 py-5 text-white">
+              Register
+            </button>
           </div>
         </nav>
       </header>
