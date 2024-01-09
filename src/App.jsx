8d4b6157760e975bedc057616fadd9e7b350a550
MatiDev <mtsz.raszeja@gmail.com>
2024-01-09 14:09:07 +0100

    dzien 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import zdjecie from "./assets/Logo.png";
+import volk from "./assets/Volks.png";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,18 +11,11 @@ function App() {
     <>
       {/* hover:m-10 */}
       <header className="m-4 flex bg-green-100 p-5 font-semibold transition-all">
-        <nav className="flex w-full justify-around bg-blue-100">
-          <div>
+        <nav className="flex w-full items-center justify-around bg-blue-100">
+          <div className="flex space-x-1">
             <div>
-              <img
-                class="h-12 w-12"
-                src="C:\Users\mtszr\Downloads\LogoAuto.jpg"
-                alt="Logo"
-              />
+              <img class="w-40" src={zdjecie} alt="Logo" />
             </div>
-            <p>
-              <span className="text-lg">CAR</span>Rental
-            </p>
           </div>
           <div className="flex space-x-6 py-8 text-center">
             <div className="">Home</div>
@@ -32,23 +27,71 @@ function App() {
           </div>
           <div className="py-5">
             <button className=" bg-white px-5 py-2.5">Sign In</button>
-            <button className=" bg-red-700 px-5 py-2.5 text-white">
+            <button className=" bg-red-700 px-5 py-2.5 text-white shadow-md shadow-black/50">
               Register
             </button>
           </div>
         </nav>
       </header>
-      <div class="mx-auto flex max-w-sm items-center space-x-4 rounded-xl bg-pink-500 p-6 shadow-lg">
+
+      {/* <div class="mx-auto flex max-w-sm items-center space-x-4 rounded-xl bg-pink-500 p-6 shadow-lg">
         <div class="shrink-0">
-          <img
-            class="h-12 w-12"
-            src="C:\Users\mtszr\Downloads\LogoAuto.jpg"
-            alt="ChitChat Logo"
-          />
+          <img class="h-12 w-12" src={zdjecie} alt="ChitChat Logo" />
         </div>
         <div>
           <div class="text-xl font-medium text-black">ChitChat</div>
           <p class="text-slate-500">You have a new message!</p>
+        </div>
+      </div> */}
+
+      {/* <div className="flex h-screen w-full flex-col items-center justify-center bg-purple-300">
+        <div className="m-2 flex h-10 w-10 items-center justify-center bg-black text-white">
+          1
+        </div>
+        <div className="m-2 flex h-10 w-10 items-center justify-center bg-black text-white">
+          2
+        </div>
+        <div className="m-2 flex h-10 w-10 items-center justify-center bg-black text-white">
+          3
+        </div>
+        <div className="m-2 flex h-10 w-10 items-center justify-center bg-black text-white">
+          4
+        </div>
+
+      </div> */}
+      <div className="flex h-screen w-full items-center justify-center bg-purple-300">
+        <div>
+          <div className="m-2 flex h-10 w-10 items-center justify-center bg-black text-white">
+            1
+          </div>
+          <div className="m-2 flex h-10 w-10 items-center justify-center bg-black text-white">
+            1
+          </div>
+          <div className="m-2 flex h-10 w-10 items-center justify-center bg-black text-white">
+            1
+          </div>
+        </div>
+        <div>
+          <div className="m-2 flex h-10 w-10 items-center justify-center bg-black text-white">
+            2
+          </div>
+          <div className="m-2 flex h-10 w-10 items-center justify-center bg-black text-white">
+            2
+          </div>
+          <div className="m-2 flex h-10 w-10 items-center justify-center bg-black text-white">
+            2
+          </div>
+        </div>
+        <div>
+          <div className="m-2 flex h-10 w-10 items-center justify-center bg-black text-white">
+            3
+          </div>
+          <div className="m-2 flex h-10 w-10 items-center justify-center bg-black text-white">
+            3
+          </div>
+          <div className="m-2 flex h-10 w-10 items-center justify-center bg-black text-white">
+            3
+          </div>
         </div>
       </div>
     </>
